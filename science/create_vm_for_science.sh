2b40aaa821b2923b69114b78df527b6b8493217d
dianshu <feishicheng2015@163.com>
2024-09-05 19:46:43 +0800

    Update create_vm_for_science.sh

--- science/create_vm_for_science.sh
+++ science/create_vm_for_science.sh
@@ -26,23 +26,16 @@ az network vnet create \
     --resource-group $RESOURCE_GROUP \
     --location $LOCATION \
     --name $VNET_NAME \
-    --address-prefixes 10.0.0.0/16 2404:f800:8000:122::/63 \
+    --address-prefixes 10.0.0.0/16 \
     --subnet-name $SUBNET_NAME \
-    --subnet-prefixes 10.0.0.0/24 2404:f800:8000:122::/64
+    --subnet-prefixes 10.0.0.0/24
 
-# create public ip, including ipv4 and ipv6
+# create public ip
 az network public-ip create \
     --resource-group $RESOURCE_GROUP \
     --name $PUBLIC_IPV4_NAME \
     --sku Standard \
     --version IPv4 \
-    --zone 1 2 3
-
-az network public-ip create \
-    --resource-group $RESOURCE_GROUP \
-    --name $PUBLIC_IPV6_NAME \
-    --sku Standard \
-    --version IPv6 \
     --zone 1 2 3
 
 # create nsg
@@ -123,25 +116,18 @@ az network nic create \
     --network-security-group $NSG_NAME \
     --public-ip-address $PUBLIC_IPV4_NAME
 
-# create ipv6 config
-az network nic ip-config create \
-    --resource-group $RESOURCE_GROUP \
-    --name $IPV6_CONFIG \
-    --nic-name $NIC_NAME \
-    --private-ip-address-version IPv6 \
-    --vnet-name $VNET_NAME \
-    --subnet $SUBNET_NAME \
-    --public-ip-address $PUBLIC_IPV6_NAME
-
 # create vm
-az vm create \
+password=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8`
+public_ip=`az vm create \
     --resource-group $RESOURCE_GROUP \
     --name $VM_NAME \
     --nics $NIC_NAME \
     --admin-username $VM_USER \
-    --authentication-type ssh \
-    --generate-ssh-keys \
+    --admin-password $password
+    --authentication-type password \
     --image $VM_IMAGE \
-    --size Standard_D2ds_v5
+    --size Standard_D2ds_v5 \
+    --query "publicIpAddress"`
 
-echo "Cmd to login: ssh $VM_USER@{PublicIP}"
+echo "Cmd to login: ssh $VM_USER@$public_ip"
+echo "password: $password"
