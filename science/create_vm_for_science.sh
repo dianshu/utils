bd9afa4f85581abcaabd5135cde83fffd597b1c7
dianshu <27887844+dianshu@users.noreply.github.com>
2025-01-15 13:38:11 +0800

    Update create_vm_for_science.sh

--- science/create_vm_for_science.sh
+++ science/create_vm_for_science.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # reference: https://learn.microsoft.com/en-us/azure/virtual-network/ip-services/create-vm-dual-stack-ipv6-cli
 
+# set -ex
+
 UNIQUE_ID=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8`
 RESOURCE_GROUP="tool-$UNIQUE_ID"
 LOCATION="SoutheastAsia"
 VNET_NAME="vnet-$UNIQUE_ID"
 SUBNET_NAME="subnet-$UNIQUE_ID"
 PUBLIC_IPV4_NAME="ipv4-$UNIQUE_ID"
+PUBLIC_IPV6_NAME="ipv6-$UNIQUE_ID"
 NSG_NAME="nsg-$UNIQUE_ID"
 NIC_NAME="nic-$UNIQUE_ID"
 IPV6_CONFIG="config-$UNIQUE_ID"
 VM_NAME="vm-$UNIQUE_ID"
 VM_USER="fei"
-VM_IMAGE="Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest"
-set -ex
+VM_IMAGE="Canonical:ubuntu-24_04-lts:server:latest"
 
 # delete resource group if exists
 az group delete -y --name $RESOURCE_GROUP || true
@@ -26,16 +28,24 @@ az network vnet create \
     --resource-group $RESOURCE_GROUP \
     --location $LOCATION \
     --name $VNET_NAME \
-    --address-prefixes 10.0.0.0/16 \
+    --address-prefixes 10.0.0.0/16 2404:f800:8000:122::/64 \
     --subnet-name $SUBNET_NAME \
-    --subnet-prefixes 10.0.0.0/24
+    --subnet-prefixes 10.0.0.0/24 2404:f800:8000:122::/64
 
-# create public ip
+# create public ipv4
 az network public-ip create \
     --resource-group $RESOURCE_GROUP \
     --name $PUBLIC_IPV4_NAME \
     --sku Standard \
     --version IPv4 \
+    --zone 1 2 3
+
+# create public ipv6
+az network public-ip create \
+    --resource-group $RESOURCE_GROUP \
+    --name $PUBLIC_IPV6_NAME \
+    --sku Standard \
+    --version IPv6 \
     --zone 1 2 3
 
 # create nsg
@@ -103,6 +113,17 @@ az network nic create \
     --network-security-group $NSG_NAME \
     --public-ip-address $PUBLIC_IPV4_NAME
 
+
+# create ipv6 config
+az network nic ip-config create \
+    --resource-group $RESOURCE_GROUP \
+    --name $IPV6_CONFIG \
+    --nic-name $NIC_NAME \
+    --private-ip-address-version IPv6 \
+    --vnet-name $VNET_NAME \
+    --subnet $SUBNET_NAME \
+    --public-ip-address $PUBLIC_IPV6_NAME
+
 # create vm
 password=`head /dev/urandom | tr -dc A-Za-z0-9.,[] | head -c 50`
 public_ip=`az vm create \
@@ -114,6 +135,7 @@ public_ip=`az vm create \
     --authentication-type password \
     --image $VM_IMAGE \
     --size Standard_D2ds_v5 \
+    --security-type TrustedLaunch \
     --query "publicIpAddress" \
     --output tsv`
 
